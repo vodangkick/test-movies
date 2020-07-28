8731ef61a7c520cbe4fc04a2ca003894338640ef
vodangkick <quang115thd@gmail.com>
2020-07-28 04:29:52 -0700

    Updates

---
+++ precache-manifest.e5edf7cb3c4a103774ecffdcb9902fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc7832253383cd2ba6a664d21372d2a",
+    "url": "/test-movies/index.html"
+  },
+  {
+    "revision": "fed8f87c949ccf583d7a",
+    "url": "/test-movies/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "915c4fb461b758a53115",
+    "url": "/test-movies/static/css/main.6708e3c7.chunk.css"
+  },
+  {
+    "revision": "fed8f87c949ccf583d7a",
+    "url": "/test-movies/static/js/2.0e8d7e74.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/test-movies/static/js/2.0e8d7e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915c4fb461b758a53115",
+    "url": "/test-movies/static/js/main.818f027d.chunk.js"
+  },
+  {
+    "revision": "94cbfc7fffaa2d2f0d2a",
+    "url": "/test-movies/static/js/runtime-main.601ed73b.js"
+  }
+]);
