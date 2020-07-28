6b0d29a0547601977ce584634525a48d12cebf82
vodangkick <quang115thd@gmail.com>
2020-07-28 05:08:49 -0700

    upte new asdsast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+// import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from 'react-apollo';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MoviesList from './pages/MoviesList';
 import MovieDetail from './pages/MovieDetail';
 
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
-const client = new ApolloClient({
-  uri: "https://ion-movies.herokuapp.com"
-});
+// const client = new ApolloClient({
+//   uri: "https://ion-movies.herokuapp.com"
+// });
 
 function App() {
   return (
     <>
       <Switch>
-        <ApolloProvider client={client}>
 
-          <Route path="/" exact component={MoviesList} />
-          <Route path="/movie/:id" exact component={MovieDetail} />
+        <Route path="/" exact component={MoviesList} />
+        <Route path="/movie/:id" exact component={MovieDetail} />
 
-        </ApolloProvider>
       </Switch>
     </>
   )
