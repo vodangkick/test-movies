8a02d221423c2db83145f20c43864a50d415603b
vodangkick <quang115thd@gmail.com>
2020-07-28 05:17:46 -0700

    upload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <HashRouter basename="/">
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
